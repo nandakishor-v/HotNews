f2b1afade1d9b9d7c5d114745b78c806cc5e091c
Nandakishor.V <nandakishor.v2020@vitbhopal.ac.in>
2023-06-11 12:58:15 +0530

    error in my device

--- app/src/main/java/com/example/hotnews/MainActivity.kt
+++ app/src/main/java/com/example/hotnews/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            HotNewsTheme {
+
+            }
 
         }
     }
--- app/src/main/java/com/example/hotnews/ui/theme/Theme.kt
+++ app/src/main/java/com/example/hotnews/ui/theme/Theme.kt
@@ -10,22 +10,20 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+   primaryContainer = Color.Black,
+    onPrimaryContainer = Color.White
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
+    primaryContainer = RoyalBlue,
+    onPrimaryContainer = Color.White
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -57,7 +55,7 @@ fun HotNewsTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.primaryContainer.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
