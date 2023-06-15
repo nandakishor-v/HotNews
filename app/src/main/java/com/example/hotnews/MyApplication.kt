4e9156addd4d740a97c69f472837fa13b2e773e3
Dyutin <dyutin333@gmail.com>
2023-06-15 18:44:07 +0530

    Implemented Dagger Hilt TODO: Fix API

---
+++ app/src/main/java/com/example/hotnews/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.example.hotnews
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication: Application() {
+
+}
---
+++ app/src/main/java/com/example/hotnews/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.example.hotnews.di
+
+import com.example.hotnews.data.remote.NewsApi
+import com.example.hotnews.data.remote.NewsApi.Companion.BASE_URL
+import com.example.hotnews.data.repository.NewsRepositoryImp
+import com.example.hotnews.domain.repository.NewsRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApi {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit.create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(newsApi: NewsApi): NewsRepository {
+        return NewsRepositoryImp(newsApi = newsApi)
+    }
+}
