accec475003c1742bbd654812eee7bd5fe2dca46
Dyutin <dyutin333@gmail.com>
2023-06-16 15:00:53 +0530

    Implemented Top App Bar

---
+++ app/src/main/java/com/example/hotnews/presentation/components/NewsScreenTopBar.kt
@@ -0,0 +1,34 @@
+package com.example.hotnews.presentation.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewsScreenTopBar(
+    onSearchIconClicked: () -> Unit
+) {
+    TopAppBar(
+        title = { Text(text = "HotNews", fontWeight = FontWeight.Bold)},
+        actions = {
+            IconButton(onClick = onSearchIconClicked) {
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
+            }
+        },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
+}
--- app/src/main/java/com/example/hotnews/presentation/news_screen/NewsScreen.kt
+++ app/src/main/java/com/example/hotnews/presentation/news_screen/NewsScreen.kt
@@ -2,29 +2,42 @@ package com.example.hotnews.presentation.news_screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.hotnews.domain.model.Article
 import com.example.hotnews.presentation.components.NewsArticleCard
+import com.example.hotnews.presentation.components.NewsScreenTopBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewsScreen(
     viewModel: NewsScreenViewModel = hiltViewModel()
 ){
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(16.dp) ){
-        items(viewModel.articles){
-           article ->
-            NewsArticleCard(
-                article = article,
-                onCardClicked = {}
-            )
-
+    Scaffold(
+        topBar = {
+            NewsScreenTopBar(onSearchIconClicked = {})
         }
+    ) { padding ->
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp),
+            modifier = Modifier.padding(padding)
+        ){
+            items(viewModel.articles){
+               article ->
+                NewsArticleCard(
+                    article = article,
+                    onCardClicked = {}
+                )
+                }
+            }
     }
 }
